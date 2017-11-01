5c17fbc2614acffa2191467200e664277df48e40
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-01 10:53:27 -0500

    3.R pt2

--- Number3.R
+++ Number3.R
@@ -6,7 +6,12 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
 #rest of your code goes here
-
+#1
 ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
   labs(x = "Adult Mass", y = "Newborn Mass") + geom_point()
+
+#2
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  labs(x = "Adult Mass", y = "Newborn Mass") +
+  scale_x_log10() + scale_y_log10() + geom_point()
 
