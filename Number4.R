de94dbf0f5096495c214096c63007641dc5e2ad4
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-01 11:39:52 -0500

    4.R pt4

--- Number4.R
+++ Number4.R
@@ -21,3 +21,11 @@ ggplot(data = dat, aes(x = F_mass)) +
   scale_x_log10() +
   geom_histogram(fill = "blue", alpha = 0.3) +
   geom_histogram(aes(x = M_mass), alpha = 0.3)
+#4
+ggplot(data = dat, aes(x = F_mass)) +
+  labs(x = "Female Mass g") +
+  scale_x_log10() +
+  geom_histogram(fill =  "blue", alpha = 0.3) +
+  geom_histogram(aes(x = M_mass), alpha = 0.3) +
+  facet_wrap(~family)
+#5
