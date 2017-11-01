ea9843339a3ff59f014d159989873d5b9fd610db
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-01 11:17:55 -0500

    4.R pt2

--- Number4.R
+++ Number4.R
@@ -10,3 +10,9 @@ dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan0
 ggplot(data = dat, aes(x = F_mass)) +
   labs(x = "Female Mass g") +
   geom_histogram()
+#2
+ggplot(data = dat, aes(x = F_mass)) +
+  labs(x = "Female Mass g") +
+  scale_x_log10() +
+  geom_histogram(fill = "blue")
+#3
