c981978fdff20e91cb7473b8166c2285677d0b0e
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-01 11:07:59 -0500

    3.R pt5

--- Number3.R
+++ Number3.R
@@ -5,7 +5,7 @@ library(tidyverse)
 dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt"), 
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
-#rest of your code goes here
+#rest of your code goes here 
 #1
 ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
   labs(x = "Adult Mass", y = "Newborn Mass") + geom_point()
@@ -23,3 +23,10 @@ ggplot(data = dat, aes(x = mass.g., y = newborn.g., color=order)) +
   labs(x = "Adult Mass", y = "Newborn Mass") +
   scale_x_log10() + scale_y_log10() + geom_point() +
   facet_wrap(~ order, nrow = 4, ncol = 5)
+#5
+  ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+    labs(x = "Adult Mass", y = "Newborn Mass") +
+    scale_x_log10() + scale_y_log10() + geom_point() +
+    facet_wrap(~ order, nrow = 4, ncol = 5) +
+    geom_smooth(method = "lm")
+  
